67217d5e1c4a02904953a72e9815b263ac644f4b
Patrick Marie <mycroft@monkeyz.eu>
2010-11-11 14:57:44 +0100

    Refactor and add some functions like follow, unfollow, get friends, etc.

--- Twitter.class.php
+++ Twitter.class.php
@@ -19,11 +19,8 @@ class Twitter
         $this->oauth_token_secret = $oauth_token_secret;
     }
 
-    public function update($message)
+    public function request($url, $method, $twitter_params)
     {
-        $url = 'http://api.twitter.com/1/statuses/update.json';
-        $method = 'POST';
-
         $oa = new OAuth($this->consumer_key, $this->consumer_secret);
         $nonce = sha1('nonce' + time());
         $params = array(
@@ -35,16 +32,85 @@ class Twitter
                     'oauth_token' => $this->oauth_token,
                   );
 
-        $twitter_params = array('status' => $message);
-
         $params = array_merge($params, $twitter_params);
 
         $signature = $oa->buildSignature($this->consumer_secret . '&' . $this->oauth_token_secret, $method, $url, $params);
         $params['oauth_signature'] = $signature;
 
-        $ret = $oa->request( $url . '?' . http_build_query($twitter_params, '', '&'), 'POST', NULL, $oa->makeAuthorization($params) );
+        $ret = $oa->request( $url . '?' . http_build_query($twitter_params, '', '&'), $method, NULL, $oa->makeAuthorization($params) );
 
         return $ret;
+
+    }
+
+    public function user_timeline()
+    {
+        $url = 'http://api.twitter.com/1/statuses/user_timeline.json';
+        $method = 'GET';
+
+        $rep = $this->request( $url, $method, array() );
+
+        return $rep;
+    }
+
+    public function update($message)
+    {
+        $url = 'http://api.twitter.com/1/statuses/update.json';
+        $method = 'POST';
+
+        $twitter_params = array('status' => $message);
+
+        $rep = $this->request( $url, $method, $twitter_params );
+
+        return $rep;
+    }
+
+    public function users_show($user_id)
+    {
+        $url = 'http://api.twitter.com/1/users/show.json';
+        $method = 'GET';
+
+        $twitter_params = array('user_id' => $user_id);
+
+        $rep = $this->request( $url, $method, $twitter_params );
+
+        return $rep;
+    }
+
+    public function follow($screen_name)
+    {
+        $url = 'http://api.twitter.com/1/friendships/create.json';
+        $method = 'POST';
+
+        $twitter_params = array('screen_name' => $screen_name);
+
+        $rep = $this->request( $url, $method, $twitter_params );
+
+        return $rep;
+    }
+
+    public function unfollow($user_id)
+    {
+        $url = 'http://api.twitter.com/1/friendships/destroy.json';
+        $method = 'POST';
+
+        $twitter_params = array('user_id' => $user_id);
+
+        $rep = $this->request( $url, $method, $twitter_params );
+
+        return $rep;
+    }
+
+    public function get_friends($screen_name)
+    {
+        $url = 'http://api.twitter.com/1/friends/ids.json';
+        $method = 'GET';
+
+        $twitter_params = array('screen_name' => $screen_name);
+
+        $rep = $this->request( $url, $method, $twitter_params );
+
+        return $rep;
     }
 
     public function getSampleFirehose()
